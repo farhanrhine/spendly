20c64d9073a84e2080db0586e91a7c15414526bf
Farhan <mohammadfarhanalam09@gmail.com>
2026-04-13 06:44:20 +0530

    update project name to spendly and upgrade Python requirement to 3.14

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# def main():
-#     print("Hello from expense-tracker!")
+def main():
+    print("Hello from spendly!")
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
